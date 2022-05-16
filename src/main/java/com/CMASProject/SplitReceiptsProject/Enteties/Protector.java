669b1680702b6582bcf98f2f53597d66c233a45b
Daniel Duarte <daniel.duarte@cmas-systems.com>
2022-05-16 16:13:48 +0100

    Protect Implementation

--- src/main/java/com/CMASProject/SplitReceiptsProject/Enteties/Protector.java
+++ src/main/java/com/CMASProject/SplitReceiptsProject/Enteties/Protector.java
@@ -1,9 +1,24 @@
 package com.CMASProject.SplitReceiptsProject.Enteties;
 
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
 
 public class Protector {
     
-    public static void protectPersonPdf(Person person, String destinationPath){
-        //TODO
-    }
+	public static void protectPersonPdf(Person person, String destinationPath){
+    	try  {
+			AccessPermission accessPermission = new AccessPermission();
+			StandardProtectionPolicy spp = new StandardProtectionPolicy("4321", person.getPassword(),accessPermission);
+			spp.setEncryptionKeyLength(128);
+			spp.setPermissions(accessPermission);
+			person.getDocument().protect(spp);
+			person.getDocument().save(destinationPath);
+			person.getDocument().close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
