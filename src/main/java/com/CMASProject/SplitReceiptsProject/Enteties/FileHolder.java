3592fa190fe3247e07694b83b829c3cc717b3781
Daniel Tomás <daniel.tomas@cmas-systems.com>
2022-05-16 14:33:52 +0100

    Finished FileHolder Class

--- src/main/java/com/CMASProject/SplitReceiptsProject/Enteties/FileHolder.java
+++ src/main/java/com/CMASProject/SplitReceiptsProject/Enteties/FileHolder.java
@@ -1,23 +1,46 @@
 package com.CMASProject.SplitReceiptsProject.Enteties;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class FileHolder {
     
     private PDDocument wagesReceipts;
-    private String nifsAndPasswords;
+    private List<String> nifsAndPasswords;
 
 
     public FileHolder(Config config) {
-        //TODO
+        String pdfPath = config.getOriginFolder()+"\\"+config.getRecieptsPdfFileName();
+        String passwordPath = config.getOriginFolder()+"\\"+config.getNamesAndPasswordsFileName();
+
+        PDFLoader(pdfPath);
+        passwordLoader(passwordPath);
     }
 
     private void PDFLoader(String path){
-        //TODO
+        try (PDDocument document = PDDocument.load(new File(path));) {
+            wagesReceipts = document;
+        } catch (Exception e) {
+            System.out.println("It was not possible to load the wages receipts pdf file. Error:"+e.getMessage()+"\nExiting program.");
+			Runtime.getRuntime().exit(2);
+        }
     }
 
     private void passwordLoader(String path){
-        //TODO
+        try{
+            List<String> lines = Collections.emptyList();
+            lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+            nifsAndPasswords = lines;
+        } catch (Exception e) {
+            System.out.println("It was not possible to load the passwords file. Error:"+e.getMessage()+"\nExiting program.");
+			Runtime.getRuntime().exit(3);
+        }
     }
 
     public PDDocument getWagesReceipts() {
@@ -27,10 +50,10 @@ public class FileHolder {
         this.wagesReceipts = wagesReceipts;
     }
 
-    public String getNifsAndPasswords() {
+    public List<String> getNifsAndPasswords() {
         return nifsAndPasswords;
     }
-    public void setNifsAndPasswords(String nifsAndPasswords) {
+    public void setNifsAndPasswords(List<String> nifsAndPasswords) {
         this.nifsAndPasswords = nifsAndPasswords;
     }
 }
