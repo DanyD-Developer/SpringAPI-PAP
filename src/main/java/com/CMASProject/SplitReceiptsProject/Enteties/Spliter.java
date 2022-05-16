86ac6c2ba6fe658a6d458f407c74a368eb4c4491
Daniel Duarte <daniel.duarte@cmas-systems.com>
2022-05-16 16:12:06 +0100

    Splitter Implementation

--- src/main/java/com/CMASProject/SplitReceiptsProject/Enteties/Spliter.java
+++ src/main/java/com/CMASProject/SplitReceiptsProject/Enteties/Spliter.java
@@ -1,12 +1,46 @@
 package com.CMASProject.SplitReceiptsProject.Enteties;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class Spliter {
     
-    public static void spliter(PDDocument document, List<Person> list){
-        //TODO
-    }
+	public static void spliter(PDDocument document, List<Person> list){
+    	try {
+			PDFTextStripper pdfStripper = new PDFTextStripper();
+			int lastNif = 0;
+			PDDocument lastDoc = null;
+			for(int i = 1; i <= document.getNumberOfPages(); i++) {
+				pdfStripper.setStartPage(i);
+				pdfStripper.setEndPage(i);
+				String text = pdfStripper.getText(document); 
+				for(int l = 0; l < list.size();l++) {
+					if (text.contains(list.get(l).getNif().toString())) {
+						PDPageTree listTree = document.getPages();
+						PDPage page = listTree.get(i-1); 
+						PDDocument doc = new PDDocument();
+						if(list.get(l).getNif() == lastNif) {
+							lastDoc.addPage(page);
+							list.get(l).setDocument(lastDoc);
+						}
+						else {
+							doc.addPage(page);
+							list.get(l).setDocument(doc);
+							lastDoc = doc;
+							lastNif = list.get(l).getNif();
+						}
+						doc.close();
+					}
+				}
+				 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
