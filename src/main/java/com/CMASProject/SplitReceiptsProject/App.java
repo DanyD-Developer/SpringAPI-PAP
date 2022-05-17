a43531367c5f6732476aec2e5a4346a1d85d97f0
Daniel Tomás <daniel.tomas@cmas-systems.com>
2022-05-17 10:10:55 +0100

    Added person creation in main

--- src/main/java/com/CMASProject/SplitReceiptsProject/App.java
+++ src/main/java/com/CMASProject/SplitReceiptsProject/App.java
@@ -1,7 +1,9 @@
 package com.CMASProject.SplitReceiptsProject;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import com.CMASProject.SplitReceiptsProject.Enteties.Config;
@@ -20,20 +22,26 @@ public class App {
 		try (FileInputStream propsInput = new FileInputStream(configFilePath)) {
 			props.load(propsInput);
 		} catch (Exception e) {
-			System.out.println("It was not possible to load the config file. Error:"+e.getMessage()+"Exiting program.");
+			System.out.println("It was not possible to load the config file. Error:"+e.getMessage()+"\nExiting program.");
 			Runtime.getRuntime().exit(1);
 		}
 
+		//Loads config
 		Config config = new Config(props);
 		
+		//Loads the files
 		FileHolder fileHolder = new FileHolder(config);
 
-		//TODO:
-		List<Person> personsList = null;
-		//..........
+		//Create each person 
+		List<Person> personsList = new ArrayList<>();
+		fileHolder.getNifsAndPasswords().stream().map((nifAndPassword) -> nifAndPassword.split("###")).forEach((nifAndPassword) -> personsList.add(new Person(Integer.parseInt(nifAndPassword[0]),nifAndPassword[1])));
+	
+		//List<String> passwords = 
+		//passwords.stream().map((x) -> x.split("###")).forEach((x) -> personsList.add(new Person(Integer.parseInt(x[0]),x[1])));
 
+		//Splits
 		Spliter.spliter(fileHolder.getWagesReceipts(), personsList);
 
-		personsList.forEach((x) -> Protector.protectPersonPdf(x, config.getDestinationFolder()));
+		personsList.forEach((person) -> {if(person.getDocument() == null) {Protector.protectPersonPdf(person, config.getDestinationFolder());}});
 	}
 }
